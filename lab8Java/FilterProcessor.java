04107657f68253343da90351388601b62f9b28f6
deadxaninside <149307956+deadxaninside@users.noreply.github.com>
2024-12-23 18:49:55 +0300

    Update FilterProcessor.java

--- lab8Java/FilterProcessor.java
+++ lab8Java/FilterProcessor.java
@@ -7,8 +7,6 @@ import java.util.stream.Collectors;
 public class FilterProcessor {
 
      // Метод с аннотацией @DataProcessor, который выполняет фильтрацию данных.
-     // @param data исходный список строк.
-     // @return список строк, содержащих слово "filter".
     @DataProcessor
     public List<String> filterData(List<String> data) {
         return data.stream() // Преобразуем список в поток данных
